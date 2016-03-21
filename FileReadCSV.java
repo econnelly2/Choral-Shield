3446f63e0e789e3bad23c95c60fca11ce4e0b908
Elliott Connelly <econnelly@stkentigernsacademy.org.uk>
2016-03-21 14:52:24 +0000

    sprint 2.1

--- FileReadCSV.java
+++ FileReadCSV.java
@@ -10,5 +10,15 @@ public class FileReadCSV
         final JFileChooser fc = new JFileChooser(currentDir);
         int returnVal = fc.showOpenDialog(null);
         File csvFile = fc.getSelectedFile();
-        
+
         char[] inBuffer = new char[5000];
+
+        fReader = new FileReader(csvFile);
+        int size = fReader.read(inBuffer);
+        fReader.close();
+
+        String fileContent = String.valueOf(inBuffer).substring(0,size);
+
+        return fileContent.split("\n");
+    }
+}
