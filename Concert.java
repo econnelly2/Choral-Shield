87411f99e6d846ec6784a0fdde240e3a9ce4d21a
Elliott Connelly <econnelly@stkentigernsacademy.org.uk>
2016-03-23 11:36:38 +0000

    SPRINT 2.2

---
+++ Concert.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+import java.io.*;
+public class Concert 
+{
+    //array of CUSTOMER objects 
+    private Customer Customerlist[];
+    //number of Customers to be calculated after reading file 
+    int noOfCustomers;
+    FileReadCSV bmiFile;
+    
+    public Concert()
+    {
+        bmiFile = new FileReadCSV();
+    }
+    //top level algorithm
+    public void processCustomer() throws IOException
+    {
+        countOKTicket();
+    }
+    public void setUpCustomerList() throws IOException
+    {
+     System.out.println("ChoralShield concert: Customer Ticket update");
+     System.out.println("** Preparing to read data file.");
+     //read file, fetch data as string array containing the rows
+     String[] dataRows = bmiFile.readCSVtable();
+     //calculate the number of cUSTOMER ROWS 
+     //update user with number of rows with membership details
+     System.out.println("**" +" rows read.\n\n");
+    }
+    public void countOKTicket()
+    {
+        //placeholder, add detail later
+    }
+}
